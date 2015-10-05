749703e128cc637ecf945fd381cd160e5a8b4234
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-10-05 13:39:08 +0100

    Update part IDs

--- english/config.php
+++ english/config.php
@@ -11,16 +11,16 @@ $access_token = '5f7hp3X6WQaIteb4BcrR1xJge7SMufZ6U3ZbCrTccBdKT2KQ3OuuaK56u2MFu5l
 // courses in the Timetable system
 $courses = array(
     // Undergraduate course
-    'english-tripos'            => new Course(6563, 'english-tripos', 'English Tripos', null, false, null),
+    'english-tripos'            => new Course(6806, 'english-tripos', 'English Tripos', null, false, null),
 
     // Graduate courses
-    'american-mphil'            => new Course(3166, 'american-mphil', 'American Literature MPhil', null, false, null),
-    'c-and-c-mphil'             => new Course(3165, 'c-and-c-mphil', 'Criticism and Culture MPhil', null, false, null),
-    'eighteenth-mphil'          => new Course(3164, 'eighteenth-mphil', '18th Century and Romantic Studies MPhil', null, false, null),
-    'modern-mphil'              => new Course(3130, 'modern-mphil', 'Modern and Contemporary MPhil', null, false, null),
-    'med-ren-mphil'             => new Course(3163, 'med-ren-mphil', 'Medieval and Renaissance Literature MPhil', null, false, null),
-    'english-research-seminars' => new Course(3126, 'research-seminar', 'Research Seminar', null, false, null),
-    'english-phd'               => new Course(3124, 'english-phd', 'English Phd', null, false, null),
+    'american-mphil'            => new Course(7825, 'american-mphil', 'American Literature MPhil', null, false, null),
+    'c-and-c-mphil'             => new Course(6520, 'c-and-c-mphil', 'Criticism and Culture MPhil', null, false, null),
+    'eighteenth-mphil'          => new Course(6518, 'eighteenth-mphil', '18th Century and Romantic Studies MPhil', null, false, null),
+    'modern-mphil'              => new Course(6351, 'modern-mphil', 'Modern and Contemporary MPhil', null, false, null),
+    'med-ren-mphil'             => new Course(6516, 'med-ren-mphil', 'Medieval and Renaissance Literature MPhil', null, false, null),
+    'english-research-seminars' => new Course(6245, 'research-seminar', 'Research Seminar', null, false, null),
+    'english-phd'               => new Course(6223, 'english-phd', 'English Phd', null, false, null),
 );
 
 ?>
