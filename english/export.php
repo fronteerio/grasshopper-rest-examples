b91e13a9e548c8f7883aeb1ba84be0fcd6c60153
Simon Gaeremynck <gaeremyncks@gmail.com>
2016-10-11 11:26:01 +0100

    Output ALL errors

--- english/export.php
+++ english/export.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Grab the model classes
 require('../php/model.php');
 // Grab the config data
